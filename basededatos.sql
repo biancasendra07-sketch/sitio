d9fe0bf9f05f7e35990b5d381265cd459194a10f
biancasendra07-sketch <biancasendra07@gmail.com>
2026-09-18 17:08:52 -0300

    sql

--- basededatos.sql
+++ basededatos.sql
@@ -1,2 +1,35 @@
 create database crepusuculo;
+create table usuarios(
+    id int primary key auto_increment,
+    nombre varchar(255) not null, 
+    email varchar(255) not null,
+    password varchar(255) not null,
+    tipo_usuario enum( 'admin', 'usuario') not null
+);
+CREATE TABLE libros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    fecha_publicacion DATE NOT NULL,
+    orden_saga INT NOT NULL,
+    sinopsis TEXT NULL
+);
 
+CREATE TABLE peliculas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    fecha_estreno DATE NOT NULL,
+    director VARCHAR(255) NOT NULL,
+    duracion_minutos INT NOT NULL,
+    sinopsis TEXT NULL,
+    numero_libro INT NULL
+);
+
+CREATE TABLE personajes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    especie ENUM('humano', 'vampiro', 'hombre lobo') NOT NULL,
+    familia VARCHAR(255) NULL,
+    descripcion TEXT NULL,
+    numero_libro INT NULL
+);
